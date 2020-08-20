8030ccc0237f72b22fad86f609b24e8f67f3fb46
notna <notnatrue@gmail.com>
2020-08-21 02:22:08 +0300

    fix controller response interface

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,20 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Response } from "express";
+import { IHouse, IPropertyForHouse } from './cards/interface';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get("/offers")
-  async getOffers(@Res() response: Response): Promise<Response> {
+  async getOffers(@Res() response: Response<IHouse[]>): Promise<Response> {
     const data = await this.appService.offers();
     return response.send(data);
   }
 
   @Get("/properties")
-  async getProperties(@Res() response: Response): Promise<Response> {
+  async getProperties(@Res() response: Response<IPropertyForHouse[]>): Promise<Response> {
     const data = await this.appService.properties();
     return response.send(data);
   }
